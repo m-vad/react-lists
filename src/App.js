9745f6824d2fbfc4050ba7f50e914b9786c849bb
Strages <m.vad95@gmail.com>
2021-08-18 14:07:34 +0300

    Added components: PostItem, PostList, MyButton, MyInput

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React, { useState } from 'react';
+import './styles/App.css';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'Posts', body: 'Description' },
+		{ id: 2, title: 'Posts 2', body: 'Description' },
+		{ id: 3, title: 'Posts 3', body: 'Description' },
+	]);
+
 	return (
 		<div className='App'>
-			<Counter />
-			<Counter />
-			<Counter />
-			<Counter />
+			<form>
+				<MyInput type='text' placeholder='Name' />
+				<MyInput type='text' placeholder='Description' />
+				<MyButton>Create post</MyButton>
+			</form>
+			<PostList posts={posts} title='List' />
 		</div>
 	);
 }
