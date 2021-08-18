be5af19b6854f6600b44acb7858669336d282c63
Strages <m.vad95@gmail.com>
2021-08-18 18:20:59 +0300

    Added deleting posts

--- src/App.js
+++ src/App.js
@@ -14,10 +14,18 @@ function App() {
 		setPosts([...posts, newPost]);
 	};
 
+	const removePost = (post) => {
+		setPosts(posts.filter((p) => p.id !== post.id));
+	};
+
 	return (
 		<div className='App'>
 			<PostForm create={createPost} />
-			<PostList posts={posts} title='List' />
+			{posts.length ? (
+				<PostList remove={removePost} posts={posts} title='List' />
+			) : (
+				<h2 style={{ textAlign: 'center' }}>Posts not found!</h2>
+			)}
 		</div>
 	);
 }
