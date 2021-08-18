33580e37fa46b93b81210fb665e4b2e3bc31de8a
Strages <m.vad95@gmail.com>
2021-08-18 17:18:17 +0300

    Added component PostForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import './styles/App.css';
-import PostItem from './components/PostItem';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -12,31 +10,13 @@ function App() {
 		{ id: 3, title: 'Posts 3', body: 'Description' },
 	]);
 
-	const [post, setPost] = useState({ title: '', body: '' });
-
-	const addNewPost = (e) => {
-		e.preventDefault();
-		setPosts([...posts, { ...post, id: Date.now() }]);
-		setPost({ title: '', body: '' });
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost]);
 	};
 
 	return (
 		<div className='App'>
-			<form>
-				<MyInput
-					value={post.title}
-					onChange={(e) => setPost({ ...post, title: e.target.value })}
-					type='text'
-					placeholder='Name'
-				/>
-				<MyInput
-					value={post.body}
-					onChange={(e) => setPost({ ...post, body: e.target.value })}
-					type='text'
-					placeholder='Description'
-				/>
-				<MyButton onClick={addNewPost}>Create post</MyButton>
-			</form>
+			<PostForm create={createPost} />
 			<PostList posts={posts} title='List' />
 		</div>
 	);
