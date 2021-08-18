5428e618b3b65ca289188557723a90b17fe11f5d
Strages <m.vad95@gmail.com>
2021-08-18 16:56:19 +0300

    Refactored creating new posts

--- src/App.js
+++ src/App.js
@@ -12,33 +12,26 @@ function App() {
 		{ id: 3, title: 'Posts 3', body: 'Description' },
 	]);
 
-	const [title, setTitle] = useState('');
-	const [body, setBody] = useState('');
+	const [post, setPost] = useState({ title: '', body: '' });
 
 	const addNewPost = (e) => {
 		e.preventDefault();
-		const newPost = {
-			id: Date.now(),
-			title,
-			body,
-		};
-		setPosts([...posts, newPost]);
-		setTitle('');
-		setBody('');
+		setPosts([...posts, { ...post, id: Date.now() }]);
+		setPost({ title: '', body: '' });
 	};
 
 	return (
 		<div className='App'>
 			<form>
 				<MyInput
-					value={title}
-					onChange={(e) => setTitle(e.target.value)}
+					value={post.title}
+					onChange={(e) => setPost({ ...post, title: e.target.value })}
 					type='text'
 					placeholder='Name'
 				/>
 				<MyInput
-					value={body}
-					onChange={(e) => setBody(e.target.value)}
+					value={post.body}
+					onChange={(e) => setPost({ ...post, body: e.target.value })}
 					type='text'
 					placeholder='Description'
 				/>
