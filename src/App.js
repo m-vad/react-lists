c2a2b53165aa589bfc51c66655b5351a66d62f27
Strages <m.vad95@gmail.com>
2021-08-21 19:53:06 +0300

    Added sorting posts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -9,6 +10,8 @@ function App() {
 		{ id: 2, title: 'Posts 2', body: 'Description' },
 		{ id: 3, title: 'Posts 3', body: 'Description' },
 	]);
+
+	const [selectedSort, setSelectedSort] = useState('');
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -18,9 +21,26 @@ function App() {
 		setPosts(posts.filter((p) => p.id !== post.id));
 	};
 
+	const sortPosts = (sort) => {
+		setSelectedSort(sort);
+		setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+	};
+
 	return (
 		<div className='App'>
 			<PostForm create={createPost} />
+			<hr style={{ margin: '1rem 0' }} />
+			<div>
+				<MySelect
+					value={selectedSort}
+					onChange={sortPosts}
+					defaultValue='Сортировка'
+					options={[
+						{ value: 'title', name: 'По названию' },
+						{ value: 'body', name: 'По описанию' },
+					]}
+				/>
+			</div>
 			{posts.length ? (
 				<PostList remove={removePost} posts={posts} title='List' />
 			) : (
