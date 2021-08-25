9202466cdec79af08b2b9ac9ccd48cf2eda1f2dc
Strages <m.vad95@gmail.com>
2021-08-25 16:15:10 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from 'components/UI/button/MyButton';
+import { usePosts } from 'hooks/usePosts';
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -15,21 +16,7 @@ function App() {
 
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
-
-	const sortedPosts = useMemo(() => {
-		if (filter.sort) {
-			return [...posts].sort((a, b) =>
-				a[filter.sort].localeCompare(b[filter.sort])
-			);
-		}
-		return posts;
-	}, [filter.sort, posts]);
-
-	const sortedAndSeachedPosts = useMemo(() => {
-		return sortedPosts.filter((post) =>
-			post.title.toLowerCase().includes(filter.query.toLocaleLowerCase())
-		);
-	}, [filter.query, sortedPosts]);
+	const sortedAndSeachedPosts = usePosts(posts, filter.sort, filter.query);
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -42,7 +29,9 @@ function App() {
 
 	return (
 		<div className='App'>
-			<MyButton style={{marginTop: '1rem'}} onClick={() => setModal(true)}>Создать пост</MyButton>
+			<MyButton style={{ marginTop: '1rem' }} onClick={() => setModal(true)}>
+				Создать пост
+			</MyButton>
 			<MyModal visable={modal} setVisable={setModal}>
 				<PostForm create={createPost} />
 			</MyModal>
