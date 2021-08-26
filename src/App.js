bd417758d1ffc342045ac5cdfc9411400c7adcab
Strages <m.vad95@gmail.com>
2021-08-26 21:04:55 +0300

    Added post service

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from 'components/UI/button/MyButton';
 import { usePosts } from 'hooks/usePosts';
 import axios from 'axios';
+import PostService from 'API/PostService';
 
 function App() {
 	const [posts, setPosts] = useState([]);
@@ -24,10 +25,8 @@ function App() {
 	};
 
 	async function fetchPosts() {
-		const response = await axios.get(
-			'https://jsonplaceholder.typicode.com/posts'
-		);
-		setPosts(response.data);
+		const posts = await PostService.getAll();
+		setPosts(posts);
 	}
 
 	const removePost = (post) => {
