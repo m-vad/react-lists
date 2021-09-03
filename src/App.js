ae997f81b67867972137e605b465e2ad45a66106
Strages <m.vad95@gmail.com>
2021-09-03 16:34:38 +0300

    Added pagination

--- src/App.js
+++ src/App.js
@@ -9,15 +9,25 @@ import { usePosts } from 'hooks/usePosts';
 import PostService from 'API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from 'utils/pages';
 
 function App() {
 	const [posts, setPosts] = useState([]);
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
+	const [totalPages, setTotalPages] = useState(0);
+	const [limit, setLimit] = useState(10);
+	const [page, setPage] = useState(1);
 	const sortedAndSeachedPosts = usePosts(posts, filter.sort, filter.query);
+
+	//TODO refact: using useMemo
+	let pagesArray = getPagesArray(totalPages);
+
 	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-		const posts = await PostService.getAll();
-		setPosts(posts);
+		const response = await PostService.getAll(limit, page);
+		setPosts(response.data);
+		const totalCount = response.headers['x-total-count'];
+		setTotalPages(getPageCount(totalCount, limit));
 	});
 
 	useEffect(() => {
@@ -31,6 +41,12 @@ function App() {
 
 	const removePost = (post) => {
 		setPosts(posts.filter((p) => p.id !== post.id));
+	};
+
+	//TODO refactoring pagination
+	const changePage = (page) => {
+		setPage(page);
+		fetchPosts();
 	};
 
 	return (
@@ -61,6 +77,17 @@ function App() {
 					title='Список'
 				/>
 			)}
+			<div className='page__wrapper'>
+				{pagesArray.map((p) => (
+					<span
+						onClick={() => changePage(p)}
+						key={p}
+						className={page === p ? 'page page__current' : 'page'}
+					>
+						{p}
+					</span>
+				))}
+			</div>
 		</div>
 	);
 }
