da58be7dd0865744390978fbdc17a66d397fa44e
Strages <m.vad95@gmail.com>
2021-09-06 08:30:07 +0300

    Refactoring: pagination

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostService from 'API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
 import { getPageCount, getPagesArray } from 'utils/pages';
+import Pagination from './components/UI/pagination/Pagination';
 
 function App() {
 	const [posts, setPosts] = useState([]);
@@ -20,19 +21,18 @@ function App() {
 	const [page, setPage] = useState(1);
 	const sortedAndSeachedPosts = usePosts(posts, filter.sort, filter.query);
 
-	//TODO refact: using useMemo
-	let pagesArray = getPagesArray(totalPages);
-
-	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-		const response = await PostService.getAll(limit, page);
-		setPosts(response.data);
-		const totalCount = response.headers['x-total-count'];
-		setTotalPages(getPageCount(totalCount, limit));
-	});
+	const [fetchPosts, isPostsLoading, postError] = useFetching(
+		async (limit, page) => {
+			const response = await PostService.getAll(limit, page);
+			setPosts(response.data);
+			const totalCount = response.headers['x-total-count'];
+			setTotalPages(getPageCount(totalCount, limit));
+		}
+	);
 
 	useEffect(() => {
-		fetchPosts();
-	}, []);
+		fetchPosts(limit, page);
+	}, [page]);
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -43,10 +43,9 @@ function App() {
 		setPosts(posts.filter((p) => p.id !== post.id));
 	};
 
-	//TODO refactoring pagination
 	const changePage = (page) => {
 		setPage(page);
-		fetchPosts();
+		fetchPosts(limit, page);
 	};
 
 	return (
@@ -77,17 +76,7 @@ function App() {
 					title='Список'
 				/>
 			)}
-			<div className='page__wrapper'>
-				{pagesArray.map((p) => (
-					<span
-						onClick={() => changePage(p)}
-						key={p}
-						className={page === p ? 'page page__current' : 'page'}
-					>
-						{p}
-					</span>
-				))}
-			</div>
+			<Pagination totalPages={totalPages} page={page} changePage={changePage} />
 		</div>
 	);
 }
